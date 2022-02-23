a870a2275bd993769fb55b6f88fa4282c7636b31
Shalaka15299 <95271779+Shalaka15299@users.noreply.github.com>
2022-02-23 14:58:15 +0530

    Add project files.

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BACKENDAPI.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string productName { get; set; }
+
+    }
+}
